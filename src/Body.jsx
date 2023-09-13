74a861c4fe13c611c49e87728bb18c780f8475c4
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-13 13:06:45 +0100

    update src/Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,33 +1,32 @@
 import React, { useEffect } from 'react';
-import Search from 'assets/Search.svg';
+import Search from './assets/Search.svg';
 
-const API_URL =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=5305712a59ce49234a0aa2a117b38249";
-  
+const API_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=5305712a59ce49234a0aa2a117b38249";
+
 const Body = () => {
-  
+
   const searchMovies = async (title) => {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-      
-      console.log(data.Search);
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
   }
-  
+
   useEffect(() => {
     searchMovies('Spiderman');
   }, []);
-  
-  return(
+
+  return (
     <div>
       <div>
         <input
-        placeholder= "What do you want to watch"
+          placeholder="What do you want to watch"
         />
-        <Search />
+        <img src={Search} alt="Search" />
       </div>
       <div>
         <p>Hi</p>
-        <img alt="Stuff"/>
+        <img alt="Stuff" />
       </div>
     </div>
   );
