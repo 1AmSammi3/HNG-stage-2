d8c6d8838914a51ade424c8d3bba72e4a1e7bf38
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-13 10:09:04 +0100

    update src/Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Card = () => {
-  
+  const getAPI = () => {
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=5305712a59ce49234a0aa2a117b38249");
+  }
   return(
     <h1>Hello World</h1>
   );
