140024d8ed8aefc42f8a17098f3296e80dd75b20
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-13 10:15:57 +0100

    update src/Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Card = () => {
   const getAPI = () => {
     fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=5305712a59ce49234a0aa2a117b38249");
+    .then(res => res.json());
+    .then(json => console.log(json))
   }
+  
+  useEffect(() = {
+    getAPI()
+  }, [])
   return(
     <h1>Hello World</h1>
   );
