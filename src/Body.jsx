2d502f4f8092f927682c96c84b4c89a40da46ff0
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-13 11:55:57 +0100

    update src/Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+const API_URL =
+  "https://api.themoviedb.org/3/movie/top_rated?apikey=5305712a59ce49234a0aa2a117b38249";
+  
 const Body = () => {
+  
+  useEffect(() => {
+    
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      
+      console.log(data);
+    }
+  }, []);
+  
   return(
     <div>
       <div>
