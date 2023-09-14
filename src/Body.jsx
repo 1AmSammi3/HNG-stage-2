9609ff7d3afbea9b3dd0315a614459f97a4e84c0
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-14 18:50:42 +0100

    update src/Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -62,7 +62,10 @@ const Body = ({link}) => {
         value= {searchTerm}
         onChange = {(e) => setSearchTerm(e.target.value)}
       />
-      {search onclick={() searchMovies(searchTerm)}}
+      <img 
+        src={search}
+        onClick={() searchMovies(searchTerm)}
+      />
     </div>
     {
       movies?.length > 0 ? 
