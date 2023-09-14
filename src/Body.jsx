0ff869f8f0c1d3496f8aa751805a103cad3ff0f6
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-14 12:20:25 +0100

    update src/Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -66,16 +66,7 @@ const getYearFromDate = (dateString) => {
         <p>Hi</p>
         <img alt="Stuff" />
      </div>
-     <div>
-        <img 
-        src={test.backdrop_path !== 'N/A' ? test.backdrop_path :
-        handleImageError} height="100px" width="70px"
-        />
-        <div>
-          <div>USA {getYearFromDate(test.release_date)}</div>
-          <h1>{test.title}</h1>
-        </div>
-     </div>
+     <MovieCard />
     </div>
   );
 }
