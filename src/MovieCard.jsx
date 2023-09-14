9db7f275e05a3069976eadde466c5666bc3f2e05
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-14 12:50:09 +0100

    update src/MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,16 +5,16 @@ const getYearFromDate = (dateString) => {
   return date.getFullYear();
 };
 
-const MovieCard = ({test}) => {
+const MovieCard = ({final}) => {
   return(
     <div>
         <img 
-        src={test.backdrop_path !== 'N/A' ? test.backdrop_path :
+        src={final.backdrop_path !== 'N/A' ? final.backdrop_path :
         handleImageError} height="100px" width="70px"
         />
         <div>
-          <div>USA {getYearFromDate(test.release_date)}</div>
-          <h1>{test.title}</h1>
+          <div>USA {getYearFromDate(final.release_date)}</div>
+          <h1>{final.title}</h1>
         </div>
    </div>
   );
